47be98387b52f038c30fd057ad572210d8d29340
Paul Kujawa <herr.kujawa@gmail.com>
2021-10-15 09:31:19 +0200

    tests in-browser modules

--- src/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { App } from "./index";
-
-describe("demo", () => {
-  it("should do something", () => {
-    expect(true).toBeTruthy();
-  });
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
-import { Button } from "@material-ui/core";
+import {
+  Button,
+  createGenerateClassName,
+  List,
+  ListItem,
+  StylesProvider,
+} from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom";
 import ImageDogeUrl from "./assets/doge.jpg"; // includes domain and port
@@ -7,25 +13,38 @@ import "./index.css";
 
 import(/* webpackPreload: true */ "lodash");
 
+const generateClassName = createGenerateClassName({
+  seed: "app",
+  disableGlobal: true,
+});
+
 const App = () => {
-    const onClick = async () => {
-        const { default: _ } = await import(
-          /* webpackChunkName: "lodash" */ "lodash"
-        );
-
-        alert(_.join(["this", "actually", "worked"], " "));
-    };
-
-    return (
-        <div className="root">
-            <Header />
-
-            <img style={{ height: "400px", width: "400px" }} src={ImageDogeUrl} />
-            <Form />
-
-            <Button variant="outlined" color="primary" onClick={() => onClick()}>Clicky</Button>
-        </div>
+  const onClick = async () => {
+    const { default: _ } = await import(
+      /* webpackChunkName: "lodash" */ "lodash"
     );
+
+    alert(_.join(["this", "actually", "worked"], " "));
+  };
+
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <div className="root">
+        {/* <Header /> */}
+
+        <img style={{ height: "400px", width: "400px" }} src={ImageDogeUrl} />
+        {/* <Form /> */}
+
+        <Button variant="outlined" color="primary" onClick={() => onClick()}>
+          Clicky
+        </Button>
+
+        <List>
+          <ListItem button></ListItem>
+        </List>
+      </div>
+    </StylesProvider>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
