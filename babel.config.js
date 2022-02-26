85b11a58a9811ada5fe593dcdc12edcf2a6a7252
Paul Kujawa <herr.kujawa@gmail.com>
2022-02-26 23:24:07 +0100

    improvements for browser support

--- babel.config.js
+++ babel.config.js
@@ -3,27 +3,28 @@ module.exports = (api) => {
   api.cache(true);
 
   return {
-    "presets": [
+    presets: [
       [
         // transpiling and polyfilling; when not configured, it would target ES5.
         // when run via Webpack, the TS-loader actually runs first, transpiling beforehand already.
         // when run via Jest, only Babel is involved, handling everything.
         "@babel/env",
         {
-          "targets": { "chrome": "78", "firefox": "78", "safari": "12", "edge": "18", "ie": "11"},
-          
+          targets:
+            "last 2 chrome major versions, last 2 edge major versions, last 2 firefox major versions, safari 13.1",
+
           // useBuiltIns and CoreJs are responsible for polyfilling.
-          // CoreJS version needs to match the one installed via NPM.
-          "useBuiltIns": "usage",
-          "corejs": "2.6.12"
-        }
+          // CoreJS version needs to match the minor version installed via NPM.
+          useBuiltIns: "usage",
+          corejs: { version: "3.20", proposals: true },
+        },
       ],
-      
+
       // When run via Webpack, TS-loader would had converted TSX to JS.
       // when run via Jest, transpiling needs happen via Babel. Hence it needs transpiler for React & TS.
       "@babel/preset-react",
-      "@babel/preset-typescript"
+      "@babel/preset-typescript",
     ],
-    "plugins": ["@babel/plugin-transform-runtime"]
-  }
-}
+    plugins: ["@babel/plugin-transform-runtime"],
+  };
+};
